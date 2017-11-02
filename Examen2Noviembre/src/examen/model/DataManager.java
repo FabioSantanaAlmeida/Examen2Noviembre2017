5fdd97fe98183ca58015c1f328d4e330b119acf9
fabio <fabio@LAPTOP-I6QTCSCE>
2017-11-02 12:26:54 +0000

    Clases Java

---
+++ Examen2Noviembre/src/examen/model/DataManager.java
@@ -0,0 +1,62 @@
+package examen.model;
+
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*import eshop.beans.Category;
+import eshop.beans.Book;
+import eshop.beans.Customer;
+import eshop.beans.CartItem;*/
+
+public class DataManager {
+  private String dbURL = "";
+  private String dbUserName = "";
+  private String dbPassword = "";
+
+  public void setDbURL(String dbURL) {
+    this.dbURL = dbURL;
+    }
+  public String getDbURL() {
+    return dbURL;
+    }
+
+  public void setDbUserName(String dbUserName) {
+    this.dbUserName = dbUserName;
+    }
+  public String getDbUserName() {
+    return dbUserName;
+    }
+
+  public void setDbPassword(String dbPassword) {
+    this.dbPassword = dbPassword;
+    }
+  public String getDbPassword() {
+    return dbPassword;
+    }
+
+  public Connection getConnection() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection(getDbURL(), getDbUserName(),
+          getDbPassword());
+      }
+    catch (SQLException e) {
+      System.out.println("Could not connect to DB: " + e.getMessage());
+      }
+    return conn;
+    }
+  public void putConnection(Connection conn) {
+    if (conn != null) {
+      try { conn.close(); }
+      catch (SQLException e) { }
+      }
+    }
+
+  //---------- Category operations ----------
+ 
+  }
